scabeen1/https-github.com-mapbox-mapbox-navigation-ios

Trigger metrics

scripts/trigger-metrics.py
@@ -22,7 +22,7 @@ def TriggerPipeline(token, commit, ci_ref):
     }
 
     # Branch in mobile-metrics repo if you want to trigger a custom pipeline
-    # data["branch"] = "test"
+    data["branch"] = "azarovalex/experiments"
 
     response = requests.post(url, auth=(token, ""), headers=headers, json=data)
 

wenge8n/mapbox-navigation-ios

scripts/trigger-metrics.py
@@ -22,7 +22,7 @@ def TriggerPipeline(token, commit, ci_ref):
     }
 
     # Branch in mobile-metrics repo if you want to trigger a custom pipeline
-    # data["branch"] = "test"
+    data["branch"] = "azarovalex/stage"
 
     response = requests.post(url, auth=(token, ""), headers=headers, json=data)
 